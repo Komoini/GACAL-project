69ccc25ed040270e40700901cb217110898c3526
Komoini <komolafeinioluwa9@gmail.com>
2023-02-28 22:20:14 +0100

    further commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const menu_btn = document.getElementById('menu_btn')
 const mobile_menu = document.getElementById('mobile_menu')
 const menu_bars = document.getElementById('menu_bars')
 const menu_close = document.getElementById('menu_close')
+const clipBoard = document.querySelector('#clipboard')
 
 menu_btn.addEventListener('click', () => {
   mobile_menu.classList.toggle('hidden')
@@ -9,3 +10,6 @@ menu_btn.addEventListener('click', () => {
   menu_close.classList.toggle('hidden')
 })
 
+clipBoard.addEventListener('click', () => {
+  clipBoard.toggleAttribute('class','fa-solid fa-circle-check ')
+});
