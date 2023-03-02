662d4c8b7a1158b286b3bbd9d9db9a1d6f1ab5d6
Komoini <komolafeinioluwa9@gmail.com>
2023-03-02 11:11:38 +0100

    further commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const menu_btn = document.getElementById('menu_btn')
 const mobile_menu = document.getElementById('mobile_menu')
 const menu_bars = document.getElementById('menu_bars')
 const menu_close = document.getElementById('menu_close')
-const clipBoard = document.querySelector('#clipboard')
+const copyAddress = document.querySelector('.copyAddress')
+const copyButton = document.querySelector('.copyButton')
+const popUp = document.querySelector('.popUp')
 
 menu_btn.addEventListener('click', () => {
   mobile_menu.classList.toggle('hidden')
@@ -10,6 +12,12 @@ menu_btn.addEventListener('click', () => {
   menu_close.classList.toggle('hidden')
 })
 
-clipBoard.addEventListener('click', () => {
-  clipBoard.toggleAttribute('class','fa-solid fa-circle-check ')
+copyButton.addEventListener('click',() => {
+  copyAddress.select();
+  copyAddress.setSelectionRange(0, 99999); //for mobile phone
+  navigator.clipboard.writeText(copyAddress.value);
+  popUp.innerHTML = `Address is copied!`    
+  setTimeout(() => {
+  popUp.innerHTML = ''    
+  }, 2000)
 });
